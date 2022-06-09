919cd7b6328d5fa8a81de3e31319fa42a72470d9
Diego Rafael Vieira <diegorafaelvieira@bol.com.br>
2022-06-08 22:19:39 -0300

    Add Aula Getters and Setters

---
+++ 03-gettersAndSetters.php
@@ -0,0 +1,45 @@
+<?php
+
+    class Login {
+        //Atributos
+        private $email;
+        private $senha;
+
+        //GET e SET
+        public function getEmail() {
+            return $this->email;
+        }
+
+        public function setEmail($e) {
+            $email = filter_var($e, FILTER_SANITIZE_EMAIL);
+            $this->email = $email;
+        }
+
+        public function getSenha() {
+            return $this->senha;
+        }
+
+        public function setSenha($s) {
+            $this->senha = $s;
+        }
+
+        //Método
+        public function Logar() {
+            if($this->email == "teste@teste.com" and $this->senha == "123456"): 
+                echo "Logado com sucesso!";
+            else:
+                echo "Dados inválidos!";
+            endif;
+        }
+    }
+
+    $logar = new Login();
+    $logar->setEmail("teste@teste.com");
+    $logar->setSenha("123456");
+    $logar->Logar();
+
+    echo "<br>";
+    echo $logar->getEmail();
+    echo "<br>";
+    echo $logar->getSenha();
+?>
